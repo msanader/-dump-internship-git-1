03600f0bb656b0953e6b6b8cf131b918f10e454c
msanader <mirna.sanader@outlook.com>
2021-11-11 19:22:24 +0100

    Created C# project treca_igra.

---
+++ treca_igra/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace treca_igra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mirna ima x pobjeda i y poraza");
+            Console.WriteLine("Karlo ima x pobjeda i y poraza");
+
+            Console.WriteLine("  |   |   ");
+            Console.WriteLine("  |   |   ");
+            Console.WriteLine("  |   |   ");
+        }
+    }
+}
