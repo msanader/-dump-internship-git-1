9ddbfb3a5ca185adb7448e2239c9ec17d4444ea3
msanader <mirna.sanader@outlook.com>
2021-11-11 19:14:38 +0100

    Created C# project prva_igra.

---
+++ prva_igra/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace prva_igra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mirna ima x pobjeda i y poraza");
+            Console.WriteLine("Karlo ima x pobjeda i y poraza");
+
+            Console.WriteLine("  |   |   ");
+            Console.WriteLine("  |   |   ");
+            Console.WriteLine("  |   |   ");
+
+
+        }
+    }
+}
