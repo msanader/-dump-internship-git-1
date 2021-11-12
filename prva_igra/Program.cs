f1d8931255aafc2ff591c45629d346fc539f534b
Karelo2705 <karlo.blaskovic@gmail.com>
2021-11-12 16:08:40 +0100

    Odigran potez

--- prva_igra/Program.cs
+++ prva_igra/Program.cs
@@ -9,7 +9,7 @@ namespace prva_igra
             Console.WriteLine("Mirna ima 1 pobjeda i 0 poraza");
             Console.WriteLine("Karlo ima 0 pobjeda i 1 poraza");
 
-            Console.WriteLine("  |   | X ");
+            Console.WriteLine("  |   | O ");
             Console.WriteLine("X | O | O ");
             Console.WriteLine("O |   | X ");
 
