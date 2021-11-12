e040ae22bde761df28a3aba310a2b99af76fb211
msanader <mirna.sanader@outlook.com>
2021-11-12 15:59:24 +0100

    Odigrani potezi.

--- prva_igra/Program.cs
+++ prva_igra/Program.cs
@@ -6,11 +6,11 @@ namespace prva_igra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mirna ima x pobjeda i y poraza");
-            Console.WriteLine("Karlo ima x pobjeda i y poraza");
+            Console.WriteLine("Mirna ima 1 pobjeda i 0 poraza");
+            Console.WriteLine("Karlo ima 0 pobjeda i 1 poraza");
 
             Console.WriteLine("  |   | X ");
-            Console.WriteLine("  | O | O ");
+            Console.WriteLine("X | O | O ");
             Console.WriteLine("O |   | X ");
 
 
