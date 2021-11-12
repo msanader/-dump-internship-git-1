7cb5df85b1bb044ba800a6379613f5560607e38a
Karelo2705 <karlo.blaskovic@gmail.com>
2021-11-12 15:32:52 +0100

    Odigran Karlov drugi potez

--- treca_igra/Program.cs
+++ treca_igra/Program.cs
@@ -9,7 +9,7 @@ namespace treca_igra
             Console.WriteLine("Mirna ima x pobjeda i y poraza");
             Console.WriteLine("Karlo ima x pobjeda i y poraza");
 
-            Console.WriteLine("  | O |   ");
+            Console.WriteLine("O | O |   ");
             Console.WriteLine("  | X |   ");
             Console.WriteLine("  |   | X ");
         }
