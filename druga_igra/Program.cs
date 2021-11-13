0eebff3818c3429c4542b6860c793fe2e9ad93ad
msanader <mirna.sanader@outlook.com>
2021-11-13 19:39:31 +0100

    Mirnini prvi potezi

--- druga_igra/Program.cs
+++ druga_igra/Program.cs
@@ -11,7 +11,7 @@ namespace druga_igra
 
             Console.WriteLine("  |   |   ");
             Console.WriteLine("  |   |   ");
-            Console.WriteLine("  |   |   ");
+            Console.WriteLine("  |   | X ");
         }
     }
 }
