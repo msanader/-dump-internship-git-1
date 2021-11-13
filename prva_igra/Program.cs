dc7d07f440ecd7484332c178b5626fa8644b9676
msanader <mirna.sanader@outlook.com>
2021-11-13 12:53:18 +0100

    molim te radi

--- prva_igra/Program.cs
+++ prva_igra/Program.cs
@@ -9,7 +9,7 @@ namespace prva_igra
             Console.WriteLine("Mirna ima 1 pobjeda i 0 poraza");
             Console.WriteLine("Karlo ima 0 pobjeda i 1 poraza");
 
-            Console.WriteLine("O |   | X ");
+            Console.WriteLine("X |   | X ");
             Console.WriteLine("X | O | O ");
             Console.WriteLine("O |   | X ");
 
