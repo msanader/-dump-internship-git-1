073eb357929c7e34c83afe3d2f690b6ec5b02bf9
Karelo2705 <karlo.blaskovic@gmail.com>
2021-11-13 20:15:39 +0100

    Karlov potez

--- druga_igra/Program.cs
+++ druga_igra/Program.cs
@@ -9,7 +9,7 @@ namespace druga_igra
             Console.WriteLine("Mirna ima x pobjeda i y poraza");
             Console.WriteLine("Karlo ima a pobjeda i b poraza");
 
-            Console.WriteLine("  |   |   ");
+            Console.WriteLine(" O|   |   ");
             Console.WriteLine("  | O |   ");
             Console.WriteLine("  |   | X ");
 
