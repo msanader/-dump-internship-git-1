91617036a3c5aa005cfb0e1b0bc983ad9ed20817
msanader <mirna.sanader@outlook.com>
2021-11-13 20:09:55 +0100

    Mirnini drugi potezi

--- druga_igra/Program.cs
+++ druga_igra/Program.cs
@@ -9,7 +9,7 @@ namespace druga_igra
             Console.WriteLine("Mirna ima x pobjeda i y poraza");
             Console.WriteLine("Karlo ima a pobjeda i b poraza");
 
-            Console.WriteLine("  |   |   ");
+            Console.WriteLine("  |   | X ");
             Console.WriteLine("  | O |   ");
             Console.WriteLine("  |   | X ");
 
